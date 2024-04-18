b00ac42af9330d827e88005792dc40ffae608953
jerrygems <shubham.debian@gmail.com>
2024-04-18 18:51:29 +0530

    update

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,9 @@
-const login = (req, resp) => { }
-const register = (req, resp) => { }
+const login = (req, resp) => {
+    console.log('logged in')
+}
+const register = (req, resp) => {
+    console.log('signed up')
+}
 
 module.exports = {
     login, register
